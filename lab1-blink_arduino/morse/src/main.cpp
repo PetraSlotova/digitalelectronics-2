9f9385c0d2a42f028b73bb5c10fcdcdfadc4c733
unknown <xsloto00@vutbr.cz>
2022-10-03 10:13:23 +0200

    lab1 update

--- lab1-blink_arduino/morse/src/main.cpp
+++ lab1-blink_arduino/morse/src/main.cpp
@@ -1,10 +1,3 @@
-# Lab 1: Petra Slotová
-
-### Morse code
-
-1. Listing of C code which repeats one "dot" and one "comma" (BTW, in Morse code it is letter `A`) on a LED. Always use syntax highlighting, meaningful comments, and follow C guidelines:
-
-```c
 
 #define LED_GREEN 13  
 #define SHORT_DELAY 250 
@@ -44,8 +37,3 @@ int main(void)
     // Will never reach this
     return 0;
 }
-```
-
-2. Scheme of Morse code application, i.e. connection of AVR device, LED, resistor, and supply voltage. The image can be drawn on a computer or by hand. Always name all components and their values!
-
-   ![your figure]()
